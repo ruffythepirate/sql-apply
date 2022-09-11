57798b3cd6eda162aa728311ba9c1a2385c67c7e
Johan Ablett-Karlsson <johan.ellfries@gmail.com>
2022-09-11 16:03:01 +0200

    feat(migration): implement test for finding migrations

--- src/migration/migration-finder.spec.ts
+++ src/migration/migration-finder.spec.ts
@@ -1,15 +1,25 @@
 import {findMigrations} from "./migration-finder";
 import {MigrationDefinition} from "./MigrationDefinition";
+import * as fs from "fs";
+
+// jest.mock("fs")
 
 it('should throw when no path is provided', async () => {
     await expect(findMigrations([])).rejects.toThrowError("No path provided");
 })
 
-// it('should return all migration files in target folder', async () => {
+it('should return all migration files in target folder', async () => {
+    fs.promises.readdir = jest.fn().mockResolvedValue(
+        [
+        {name: "src/migration/V1__Create_table.sql", isFile: () => true},
+        {name: "src/migration/V2__Create_table.sql", isFile: () => true},
+        {name: "src/migration/V3__Create_table.sql", isFile: () => true},
+        ]
+    )
 
-    // await expect(findMigrations(["src/migration"])).resolves.toEqual([
-    //     new MigrationDefinition("src/migration/V1__Create_table.sql", 1, "Create_table"),
-    //     new MigrationDefinition("src/migration/V2__Create_table.sql", 2, "Create_table"),
-    //     new MigrationDefinition("src/migration/V3__Create_table.sql", 3, "Create_table"),
-    //     ]);
-// })
+    await expect(findMigrations(["src/migration"])).resolves.toEqual([
+        new MigrationDefinition("src/migration/V1__Create_table.sql", 1, "Create_table"),
+        new MigrationDefinition("src/migration/V2__Create_table.sql", 2, "Create_table"),
+        new MigrationDefinition("src/migration/V3__Create_table.sql", 3, "Create_table"),
+        ]);
+})
